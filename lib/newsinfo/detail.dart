7cc8112180d49b944a50ef13b552b7ded246d8bb
yasinthon04 <yasinthon_r@cmu.ac.th>
2023-03-08 14:32:50 +0700

    add news detail in each one and some decorate

---
+++ lib/newsinfo/detail.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Detailspage extends StatefulWidget {
+  final v1, v2, v3, v4;
+  Detailspage(this.v1, this.v2, this.v3, this.v4);
+
+  @override
+  _DetailspageState createState() => _DetailspageState();
+}
+
+class _DetailspageState extends State<Detailspage> {
+  var _v1, _v2, _v3, _v4;
+  @override
+  void initState() {
+    super.initState();
+    _v1 = widget.v1;
+    _v2 = widget.v2;
+    _v3 = widget.v3;
+    _v4 = widget.v4;
+  }
+
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Details"),
+    ),
+    body: Container(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              _v1,
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              _v2,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Image.network(
+              _v3,
+              height: 200,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              _v4,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+}
--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../newsinfo/detail.dart';
+
 class NewsPage extends StatefulWidget {
-  const NewsPage({ Key? key}) : super(key: key);
+  const NewsPage({Key? key}) : super(key: key);
 
   @override
   _NewsPageState createState() => _NewsPageState();
@@ -12,12 +16,105 @@ class NewsPage extends StatefulWidget {
 class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Center(
-          child: Text("News Page"),
-          ),
-      ],
-    );
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.all(20),
+            child: FutureBuilder(
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var data = json.decode(snapshot.data.toString());
+                  return ListView.builder(
+                      itemBuilder: (BuildContext context, int index) {
+                        return MyBox(
+                            data[index]['title'],
+                            data[index]['subtitle'],
+                            data[index]['img_url'],
+                            data[index]['detail']);
+                      },
+                      itemCount: data.length);
+                } else if (snapshot.hasError) {
+                  return Center(child: Text("Error: ${snapshot.error}"));
+                }
+                // if snapshot.data is null or still loading, return a progress indicator
+                return Center(child: CircularProgressIndicator());
+              },
+              future:
+                  DefaultAssetBundle.of(context).loadString("assets/data.json"),
+            )));
   }
+
+  Widget MyBox(String title, String subtitle, String img_url, String detail) {
+    var v1, v2, v3, v4;
+    v1 = title;
+    v2 = subtitle;
+    v3 = img_url;
+    v4 = detail;
+    return Container(
+    margin: EdgeInsets.only(top: 20),
+    padding: EdgeInsets.all(20),
+    height: 150,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          spreadRadius: 2,
+          blurRadius: 5,
+          offset: Offset(0, 2), // changes position of shadow
+        ),
+      ],
+      image: DecorationImage(
+        image: NetworkImage(img_url),
+        fit: BoxFit.cover,
+        colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.5), BlendMode.darken),
+      ),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          subtitle,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.white,
+          ),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: ElevatedButton(
+              onPressed: () {
+                print("next page >>");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        Detailspage(title, subtitle, img_url, detail),
+                  ),
+                );
+              },
+              child: Text("Read more"),
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 3, 91, 255),
+                onPrimary: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
 }
