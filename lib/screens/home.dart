cca7be21edf13890229efd9ab0dd09b10c01c433
yasinthon04 <yasinthon_r@cmu.ac.th>
2023-03-10 16:33:19 +0700

    make search

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:emc/info/contactdetail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,86 +7,6 @@ import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   List<Map<String, dynamic>> items = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadData();
-//   }
-
-//   Future<void> loadData() async {
-//   try {
-//     final file = await rootBundle.loadString('assets/contactdata.json');
-//     final data = jsonDecode(file).cast<Map<String, dynamic>>();
-//     setState(() {
-//       items = data;
-//     });
-//   } catch (e) {
-//     print('Error loading data: $e');
-//   }
-//   }
-
-//   void makeCall(String phoneNumber) async {
-//     final url = 'tel:$phoneNumber';
-//     if (await canLaunch(url)) {
-//       await launch(url);
-//     } else {
-//       throw 'Could not launch $url';
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // return Scaffold(
-//     //   body: ListView.builder(
-//     //     itemBuilder: (BuildContext context, int index) {
-//     //       final item = items[index];
-//     //       return Card(
-//     //         child: ListTile(
-//     //           leading: Icon(
-//     //             Icons.person,
-//     //             color: Color.fromARGB(255, 0, 0, 0),
-//     //           ),
-//     //           title: Text(item['name']),
-//     //           subtitle: Text('${item['tell']}'),
-//     //           onTap: () {
-//     //             makeCall(item['tell']);
-//     //           },
-              
-//     //         ),
-//     //       );
-//     //     },
-//     //     itemCount: items.length,
-//     //   ),
-//     // );
-//     return MaterialApp(
-//   home: DefaultTabController(
-//     length: 3,
-//     child: Scaffold(
-//       appBar: AppBar(
-//         bottom: const TabBar(
-//           tabs: [
-//             Tab(icon: Icon(Icons.directions_car)),
-//             Tab(icon: Icon(Icons.directions_transit)),
-//             Tab(icon: Icon(Icons.directions_bike)),
-//           ],
-//         ),
-//       ),
-//     ),
-//   ),
-// );
-//   }
-// }
 
 void main() {
   runApp(const TabBarDemo());
@@ -99,11 +20,101 @@ void _launchPhoneApp(String phoneNumber) async {
   }
 }
 
+class ContactSearch extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Perform the search and display the results
+    final List<String> contacts = [
+      'John Smith',
+      'Jane Doe',
+      'Bob Johnson',
+      'Mary Smith',
+      'Tom Jones',
+      'Sarah Wilson',
+    ]; // replace with your actual list of contacts
+    final List<String> searchResults = contacts
+        .where((contact) => contact.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return ListView.builder(
+      itemCount: searchResults.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: Icon(Icons.person),
+          title: Text(searchResults[index]),
+          subtitle: Text("Contact"),
+          trailing: Icon(Icons.phone),
+          onTap: () async {
+            String number =
+                '+12345567890'; // replace with the actual phone number
+            launch('tel://$number');
+            await FlutterPhoneDirectCaller.callNumber(number);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Display suggestions as the user types in the search bar
+    final List<String> contacts =
+        []; // replace with your actual list of contacts
+    final List<String> searchResults = query.isEmpty
+        ? contacts // Show all contacts when query is empty
+        : contacts
+            .where((contact) =>
+                contact.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: searchResults.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: Icon(Icons.person),
+          title: Text(searchResults[index]),
+          subtitle: Text("Contact"),
+          trailing: Icon(Icons.phone),
+          onTap: () async {
+            String number =
+                '+12345567890'; // replace with the actual phone number
+            launch('tel://$number');
+            await FlutterPhoneDirectCaller.callNumber(number);
+          },
+        );
+      },
+    );
+  }
+}
+
 class TabBarDemo extends StatelessWidget {
   const TabBarDemo({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     final number = '+12345567890';
 
     return MaterialApp(
@@ -111,7 +122,7 @@ class TabBarDemo extends StatelessWidget {
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
-          backgroundColor: Color.fromARGB(255, 237, 109, 109), 
+          backgroundColor: Color.fromARGB(255, 237, 109, 109),
           body: Column(
             children: [
               TabBar(
@@ -119,6 +130,34 @@ class TabBarDemo extends StatelessWidget {
                   Tab(text: "Emergency Contacts"),
                   Tab(text: "Personal Contacts"),
                 ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    color: Colors.white,
+                    child: GestureDetector(
+                      onTap: () {
+                        showSearch(
+                          context: context,
+                          delegate: ContactSearch(),
+                        );
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 13),
+                        child: Row(
+                          children: [
+                            Icon(Icons.search),
+                            SizedBox(width: 10),
+                            Text('Search contacts'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
               Expanded(
                 child: TabBarView(
@@ -132,12 +171,20 @@ class TabBarDemo extends StatelessWidget {
                               title: Text("John Smith"),
                               subtitle: Text("Emergency Contact"),
                               trailing: Icon(Icons.phone),
-                              onTap: () async {
-                                launch('tel://$number');
+                              // onTap: () {
+                              //   Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => ContactDetail(v1, v2, v3, v4)),
+                              //   );
+                              // },
+                              // onTap: () async {
+                              //   launch('tel://$number');
 
-                                await FlutterPhoneDirectCaller.callNumber(number);
-                                // Do something when the user taps the ListTile
-                              },
+                              //   await FlutterPhoneDirectCaller.callNumber(
+                              //       number);
+                              //   // Do something when the user taps the ListTile
+                              // },
                             ),
                           ),
                           Card(
